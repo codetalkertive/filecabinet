e68f72f9442569c8221a9307367cd9d8d56fdae7
codetalkertive <codetalkertive@gmail.com>
2020-05-29 14:05:44 +0100

    form-based feature

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,11 +3,28 @@ class DocsController < ApplicationController
   end
 
   def new
+  	@doc = Doc.new
+  end
+
+  def create
+  	@doc = Doc.create(doc_params)
+  	if @doc.save
+  		redirect_to @doc
+  	else
+  		render :new
+  	end
   end
 
   def show
+  	@doc = Doc.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def doc_params
+  	params.require(:doc).permit(:title, :content)
   end
 end
